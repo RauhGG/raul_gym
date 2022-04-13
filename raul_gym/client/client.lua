2ed33ba0af89002925c6e65f74c9fc0274f6210b
RaulGG <66495931+RauhGG@users.noreply.github.com>
2022-04-13 19:29:28 +0200

    Update client.lua

--- raul_gym/client/client.lua
+++ raul_gym/client/client.lua
@@ -1,11 +1,11 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-
 -- Blip
 
 local blips = {
 	{title="GYM", colour=9, id=311, x = -1201.88, y = -1570.146, z = 4.608062}
 }
+
 
 CreateThread(function()
 	for _, info in pairs(blips) do
@@ -20,217 +20,6 @@ CreateThread(function()
 	  EndTextCommandSetBlipName(info.blip)
 	end
  end)
-
---Barra 1
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1198.868,-1574.603, 4.609806)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer pesas", vector3(-1198.868,-1574.603,4.609806))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)
-				TaskStartScenarioInPlace(ply, "world_human_muscle_free_weights", 0, true)
-				Citizen.Wait(30000)
-				exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
-				ClearPedTasksImmediately(ply)
-				end
-			end
-			Wait(_sleep)
-	end
-end)
--- Barra 2
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1196.824, -1573.208, 4.612616)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer pesas", vector3(-1196.824, -1573.208, 4.612616))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)
-				TaskStartScenarioInPlace(ply, "world_human_muscle_free_weights", 0, true)
-				Citizen.Wait(30000)
-				exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
-				ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end
-end)
--- Barra 3
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1202.909, -1565.074, 4.611889)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer pesas", vector3(-1202.909, -1565.074, 4.611889))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)
-				TaskStartScenarioInPlace(ply, "world_human_muscle_free_weights", 0, true)
-				Citizen.Wait(30000)
-				exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
-				ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end
-end)
--- Dominadas 1
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1200.057, -1571.131, 4.609499)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer dominadas", vector3(-1200.057, -1571.131, 4.609499))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)					
-					TaskStartScenarioInPlace(ply, "prop_human_muscle_chin_ups", 0, true)
-					Citizen.Wait(30000)
-					exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
-					ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end	
-end)
--- Dominadas 2
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1204.673, -1564.556, 4.609456)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer dominadas", vector3(-1204.673, -1564.556, 4.609456))
-			if IsControlJustPressed(0, 38) then
-					TaskStartScenarioInPlace(ply, "prop_human_muscle_chin_ups", 0, true)
-					Citizen.Wait(30000)
-					exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
-					ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end	
-end)
--- Flexiones 1
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1207.046, -1562.992, 4.607988)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer flexiones", vector3(-1207.046, -1562.992, 4.607988))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)					
-					TaskStartScenarioInPlace(ply, "world_human_push_ups", 0, true)
-					Citizen.Wait(30000)
-					exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
-					exports["gamz-skillsystem"]:UpdateSkill("Stamina", 1)
-					ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end	
-end)
--- Flexiones 2
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1204.32, -1560.859, 4.614558)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer flexiones", vector3(-1204.32, -1560.859, 4.614558))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)					
-					TaskStartScenarioInPlace(ply, "world_human_push_ups", 0, true)
-					Citizen.Wait(30000)
-					exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
-					exports["gamz-skillsystem"]:UpdateSkill("Stamina", 1)
-					ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end	
-end)
--- Yoga 1
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1214.897, -1544.653, 4.705237)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Yoga", vector3(-1214.897, -1544.653, 4.705237))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)						
-				TaskStartScenarioInPlace(ply, "world_human_yoga", 0, true)
-				Citizen.Wait(30000)
-				exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 1)
-				ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end	
-end)
--- Yoga 2
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1225.866, -1547.199, 4.610762)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Yoga", vector3(-1225.866, -1547.199, 4.610762))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)						
-				TaskStartScenarioInPlace(ply, "world_human_yoga", 0, true)
-				Citizen.Wait(30000)
-				exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 1)
-				ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end	
-end)
--- Yoga 3
-CreateThread(function()
-	while true do
-		local _sleep = 1000
-		local ply = PlayerPedId()
-		local plycoords = GetEntityCoords(ply)
-		if #(plycoords - vector3(-1222.756, -1547.993, 4.635444)) < 1 then
-			_sleep = 0
-			ESX.ShowFloatingHelpNotification("[~r~E~w~] Yoga", vector3(-1222.756, -1547.993, 4.635444))
-			if IsControlJustPressed(0, 38) then
-				ESX.ShowNotification("Preparando el ejercicio...")
-				Citizen.Wait(1000)						
-				TaskStartScenarioInPlace(ply, "world_human_yoga", 0, true)
-				Citizen.Wait(30000)
-				exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 1)
-				ClearPedTasksImmediately(ply)
-				end
-			end
-		Wait(_sleep)
-	end	
-end)
 
 
 -- Shop
@@ -275,3 +64,98 @@ tienda = function()
 		menu.close()
 	end)
 end
+
+
+
+CreateThread(function()
+	while true do
+		local _sleep = 1000
+		local ply = PlayerPedId()
+		local plycoords = GetEntityCoords(ply)
+		for k, v in pairs(Config.flexiones) do
+			if #(plycoords - v) < 1 then
+				_sleep = 0
+				ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer flexiones", v)
+				if IsControlJustPressed(0, 38) then
+					ESX.ShowNotification("Preparando el ejercicio...")
+					Citizen.Wait(1000)					
+						TaskStartScenarioInPlace(ply, "world_human_push_ups", 0, true)
+						Citizen.Wait(30000)
+						exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
+						exports["gamz-skillsystem"]:UpdateSkill("Resistencia", 1)
+						ClearPedTasksImmediately(ply)
+					end
+			end
+		end
+		Wait(_sleep)
+	end
+end)
+
+CreateThread(function()
+	while true do
+		local _sleep = 1000
+		local ply = PlayerPedId()
+		local plycoords = GetEntityCoords(ply)
+		for k, v in pairs(Config.dominadas) do
+			if #(plycoords - v) < 1 then
+				_sleep = 0
+				ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer dominadas", v)
+				if IsControlJustPressed(0, 38) then
+					ESX.ShowNotification("Preparando el ejercicio...")
+					Citizen.Wait(1000)					
+						TaskStartScenarioInPlace(ply, "prop_human_muscle_chin_ups", 0, true)
+						Citizen.Wait(30000)
+						exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
+						ClearPedTasksImmediately(ply)
+					end
+				end
+		end
+		Wait(_sleep)
+	end
+end)
+
+CreateThread(function()
+	while true do
+		local _sleep = 1000
+		local ply = PlayerPedId()
+		local plycoords = GetEntityCoords(ply)
+		for k, v in pairs(Config.pesas) do
+			if #(plycoords - v) < 1 then
+				_sleep = 0
+				ESX.ShowFloatingHelpNotification("[~r~E~w~] Hacer pesas", v)
+				if IsControlJustPressed(0, 38) then
+					ESX.ShowNotification("Preparando el ejercicio...")
+					Citizen.Wait(1000)
+					TaskStartScenarioInPlace(ply, "world_human_muscle_free_weights", 0, true)
+					Citizen.Wait(30000)
+					exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 2)
+					ClearPedTasksImmediately(ply)
+					end
+				end
+		end
+		Wait(_sleep)
+	end
+end)
+
+CreateThread(function()
+	while true do
+		local _sleep = 1000
+		local ply = PlayerPedId()
+		local plycoords = GetEntityCoords(ply)
+		for k, v in pairs(Config.yoga) do
+			if #(plycoords - v) < 1 then
+				_sleep = 0
+				ESX.ShowFloatingHelpNotification("[~r~E~w~] Yoga", v)
+				if IsControlJustPressed(0, 38) then
+					ESX.ShowNotification("Preparando el ejercicio...")
+					Citizen.Wait(1000)						
+					TaskStartScenarioInPlace(ply, "world_human_yoga", 0, true)
+					Citizen.Wait(30000)
+					exports["gamz-skillsystem"]:UpdateSkill("Fuerza", 1)
+					ClearPedTasksImmediately(ply)
+					end
+				end
+		end
+		Wait(_sleep)
+	end
+end)
